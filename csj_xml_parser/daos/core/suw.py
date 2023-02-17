c09f42f68014f90f3a5d081a3217efbf3c5447a9
shimamorieiki <aifore.2270@gmail.com>
2023-02-17 10:43:11 +0000

    add: suw

---
+++ csj_xml_parser/daos/core/suw.py
@@ -0,0 +1,118 @@
+"""[summary]
+Returns:
+    [type]: [description]
+"""
+
+
+from xml.etree.ElementTree import Element
+
+from ...daos.core.trans_suw import DaoTransSuw
+from ...dtos.core.suw import DtoSuwCore
+from ...dtos.core.trans_suw import DtoTransSuw
+from .base import DaoBase
+
+
+class DaoSuwCore(DaoBase):
+    """[summary]
+    CSJのIPU単位を扱うクラス
+    """
+
+    def parse(self, suw_element: Element) -> DtoSuwCore:
+        """[summary]
+        .xmlファイルからsuwのリストを取得する
+        Args:
+            file (str): xmlファイルのパス
+        Returns:
+            suw_list (list[str]): suwのリスト
+        """
+        suw_id_str: str | None = suw_element.get("SUWID")
+        column_id_str: str | None = suw_element.get("ColumnID")
+        dictionary_form: str | None = suw_element.get("SUWDictionaryForm")
+        lemma: str | None = suw_element.get("SUWLemma")
+        pos: str | None = suw_element.get("SUWPOS")
+        conjugate_type: str | None = suw_element.get("ConjugateType")
+        conjugate_type2: str | None = suw_element.get("ConjugateType2")
+        conjugate_form: str | None = suw_element.get("ConjugateForm")
+        conjugate_form2: str | None = suw_element.get("ConjugateForm2")
+        misc_pos_info1: str | None = suw_element.get("SUWMiscPOSInfo1")
+        misc_pos_info2: str | None = suw_element.get("SUWMiscPOSInfo2")
+        misc_pos_info3: str | None = suw_element.get("SUWMiscPOSInfo3")
+        plain_orthographic_transcription: str | None = suw_element.get(
+            "PlainOrthographicTranscription"
+        )
+        orthographic_transcription: str | None = suw_element.get(
+            "OrthographicTranscription"
+        )
+        phonetic_transcription: str | None = suw_element.get("PhoneticTranscription")
+        clause_unit_id_str: str | None = suw_element.get("ClauseUnitID")
+        clause_boundary_label: str | None = suw_element.get("ClauseBoundaryLabel")
+        cu_operation_sign: str | None = suw_element.get("CU_OperationSign")
+        cu_pre_bracket: str | None = suw_element.get("CU_PreBracket")
+        cu_post_bracket: str | None = suw_element.get("CU_PostBracket")
+        cu_obligate_comment: str | None = suw_element.get("CU_ObligateComment")
+        dep_bunsetu_unit_id_str: str | None = suw_element.get("Dep_BunsetsuUnitID")
+        dep_modifiee_bunsetu_unit_id_str: str | None = suw_element.get(
+            "Dep_ModifieeBunsetsuUnitID"
+        )
+        dep_obligate_comment: str | None = suw_element.get("Dep_obligateComment")
+        dep_label: str | None = suw_element.get("Dep_Label")
+        trans_suw_elements: list[Element] = list(suw_element.iter("TransSUW"))
+
+        if len(trans_suw_elements) != 1:
+            raise ValueError("can't find Suw")
+
+        trans_suw: DtoTransSuw = DaoTransSuw().parse(
+            trans_suw_element=trans_suw_elements[0]
+        )
+
+        suw_id: int | None = (
+            int(suw_id_str, base=10) if suw_id_str is not None else None
+        )
+
+        column_id: int | None = (
+            int(column_id_str, base=10) if column_id_str is not None else None
+        )
+
+        clause_unit_id: int | None = (
+            int(clause_unit_id_str, base=10) if clause_unit_id_str is not None else None
+        )
+
+        dep_bunsetu_unit_id: int | None = (
+            int(dep_bunsetu_unit_id_str, base=10)
+            if dep_bunsetu_unit_id_str is not None
+            else None
+        )
+        dep_modifiee_bunsetu_unit_id: int | None = (
+            int(dep_modifiee_bunsetu_unit_id_str, base=10)
+            if dep_modifiee_bunsetu_unit_id_str is not None
+            else None
+        )
+
+        return DtoSuwCore(
+            suw_id=suw_id,
+            column_id=column_id,
+            dictionary_form=dictionary_form,
+            lemma=lemma,
+            pos=pos,
+            conjugate_type=conjugate_type,
+            conjugate_type2=conjugate_type2,
+            conjugate_form=conjugate_form,
+            conjugate_form2=conjugate_form2,
+            misc_pos_info1=misc_pos_info1,
+            misc_pos_info2=misc_pos_info2,
+            misc_pos_info3=misc_pos_info3,
+            plain_orthographic_transcription=plain_orthographic_transcription,
+            orthographic_transcription=orthographic_transcription,
+            phonetic_transcription=phonetic_transcription,
+            clause_unit_id=clause_unit_id,
+            clause_boundary_label=clause_boundary_label,
+            cu_operation_sign=cu_operation_sign,
+            cu_pre_bracket=cu_pre_bracket,
+            cu_post_bracket=cu_post_bracket,
+            cu_obligate_comment=cu_obligate_comment,
+            dep_bunsetu_unit_id=dep_bunsetu_unit_id,
+            dep_modifiee_bunsetu_unit_id=dep_modifiee_bunsetu_unit_id,
+            dep_obligate_comment=dep_obligate_comment,
+            dep_label=dep_label,
+            trans_suw=trans_suw,
+        )
--- csj_xml_parser/dtos/core/suw.py
+++ csj_xml_parser/dtos/core/suw.py
@@ -1,8 +1,6 @@
-"""_summary_
+"""_summary_"""
 
-Returns:
-    _type_: _description_
-"""
+from .trans_suw import DtoTransSuw
 
 
 class DtoSuwCore:
@@ -10,33 +8,63 @@ class DtoSuwCore:
     suwタグの要素
     """
 
-    def __init__(self):
-        self.suw_id: str = None
-        self.column_id: int = None
-        self.dictionary_form: str = None
-        self.lemma: str = None
-        self.pos: str = None
-        self.conjugate_type: str = None
-        self.conjugate_type2: str = None
-        self.conjugate_form: str = None
-        self.conjugate_form2: str = None
-        self.misc_pos_info1: str = None
-        self.misc_pos_info2: str = None
-        self.misc_pos_info3: str = None
-        self.plain_orthographic_transcription: str = None
-        self.orthographic_transcription: str = None
-        self.phonetic_transcription: str = None
-        self.clause_unit_id: int = None
-        self.clause_boundary_label: str = None
-        self.cu_operation_sign: str = None
-        self.cu_pre_bracket: str = None
-        self.cu_post_bracket: str = None
-        self.cu_obligate_comment: str = None
-        self.dep_bunsetu_unit_id: int = None
-        self.dep_modifiee_bunsetu_unit_id: int = None
-        self.dep_obligate_comment: str = None
-        self.dep_label: str = None
-        self.ipu_pause: str = None
+    def __init__(
+        self,
+        suw_id: int | None,
+        column_id: int | None,
+        dictionary_form: str | None,
+        lemma: str | None,
+        pos: str | None,
+        conjugate_type: str | None,
+        conjugate_type2: str | None,
+        conjugate_form: str | None,
+        conjugate_form2: str | None,
+        misc_pos_info1: str | None,
+        misc_pos_info2: str | None,
+        misc_pos_info3: str | None,
+        plain_orthographic_transcription: str | None,
+        orthographic_transcription: str | None,
+        phonetic_transcription: str | None,
+        clause_unit_id: int | None,
+        clause_boundary_label: str | None,
+        cu_operation_sign: str | None,
+        cu_pre_bracket: str | None,
+        cu_post_bracket: str | None,
+        cu_obligate_comment: str | None,
+        dep_bunsetu_unit_id: int | None,
+        dep_modifiee_bunsetu_unit_id: int | None,
+        dep_obligate_comment: str | None,
+        dep_label: str | None,
+        trans_suw: DtoTransSuw,
+    ):
+        self.suw_id: int | None = suw_id
+        self.column_id: int | None = column_id
+        self.dictionary_form: str | None = dictionary_form
+        self.lemma: str | None = lemma
+        self.pos: str | None = pos
+        self.conjugate_type: str | None = conjugate_type
+        self.conjugate_type2: str | None = conjugate_type2
+        self.conjugate_form: str | None = conjugate_form
+        self.conjugate_form2: str | None = conjugate_form2
+        self.misc_pos_info1: str | None = misc_pos_info1
+        self.misc_pos_info2: str | None = misc_pos_info2
+        self.misc_pos_info3: str | None = misc_pos_info3
+        self.plain_orthographic_transcription: str | None = (
+            plain_orthographic_transcription
+        )
+        self.orthographic_transcription: str | None = orthographic_transcription
+        self.phonetic_transcription: str | None = phonetic_transcription
+        self.clause_unit_id: int | None = clause_unit_id
+        self.clause_boundary_label: str | None = clause_boundary_label
+        self.cu_operation_sign: str | None = cu_operation_sign
+        self.cu_pre_bracket: str | None = cu_pre_bracket
+        self.cu_post_bracket: str | None = cu_post_bracket
+        self.cu_obligate_comment: str | None = cu_obligate_comment
+        self.dep_bunsetu_unit_id: int | None = dep_bunsetu_unit_id
+        self.dep_modifiee_bunsetu_unit_id: int | None = dep_modifiee_bunsetu_unit_id
+        self.dep_obligate_comment: str | None = dep_obligate_comment
+        self.dep_label: str | None = dep_label
+        self.trans_suw: DtoTransSuw = trans_suw
 
     def to_list(self):
         """_summary_
@@ -70,39 +98,8 @@ class DtoSuwCore:
             self.dep_modifiee_bunsetu_unit_id,
             self.dep_obligate_comment,
             self.dep_label,
-            self.ipu_pause,
+            self.trans_suw.to_list(),
         ]
-
-    def set_dummy_data(self):
-        """_summary_
-        適当なデータをセットする
-        """
-        self.suw_id = ""
-        self.column_id = 0
-        self.dictionary_form = ""
-        self.lemma = ""
-        self.pos = ""
-        self.conjugate_type = ""
-        self.conjugate_type2 = ""
-        self.conjugate_form = ""
-        self.conjugate_form2 = ""
-        self.misc_pos_info1 = ""
-        self.misc_pos_info2 = ""
-        self.misc_pos_info3 = ""
-        self.plain_orthographic_transcription = ""
-        self.orthographic_transcription = ""
-        self.phonetic_transcription = ""
-        self.clause_unit_id = 0
-        self.clause_boundary_label = ""
-        self.cu_operation_sign = ""
-        self.cu_pre_bracket = ""
-        self.cu_post_bracket = ""
-        self.cu_obligate_comment = ""
-        self.dep_bunsetu_unit_id = 0
-        self.dep_modifiee_bunsetu_unit_id = 0
-        self.dep_obligate_comment = ""
-        self.dep_label = ""
-        self.ipu_pause = ""
 
     def __str__(self):
         return ", ".join([str(i) for i in self.to_list()])
