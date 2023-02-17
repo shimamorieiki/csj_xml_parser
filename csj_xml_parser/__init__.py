c1fe9a071cec7ba1a285c4d5c440936c1e74e5c2
shimamorieiki <aifore.2270@gmail.com>
2023-02-17 21:32:07 +0900

    v0.2.2

--- csj_xml_parser/__init__.py
+++ csj_xml_parser/__init__.py
@@ -1,4 +1,19 @@
-from .daos import *
-from .dtos import *
+from .daos.core.base import *
+from .daos.core.comment import *
+from .daos.core.ipu import *
+from .daos.core.luw import *
+from .daos.core.mora import *
+from .daos.core.suw import *
+from .daos.core.talk import *
+from .daos.core.talk_comment import *
+from .daos.core.trans_suw import *
+from .dtos.core.comment import *
+from .dtos.core.ipu import *
+from .dtos.core.luw import *
+from .dtos.core.mora import *
+from .dtos.core.suw import *
+from .dtos.core.talk import *
+from .dtos.core.talk_comment import *
+from .dtos.core.trans_suw import *
 
-__version__ = "0.2.0"
+__version__ = "0.2.2"
